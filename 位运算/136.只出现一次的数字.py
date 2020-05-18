f2d3dab39e354cf37ebf3a619e8d2b3c61083552
huang <1159986871@qq.com>
2020-05-18 18:58:43 +0800

    解决127问题

--- 位运算/136.只出现一次的数字.py
+++ 位运算/136.只出现一次的数字.py
@@ -3,10 +3,14 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        for x, y in enumerate(nums):
-            if y not in nums[:x] + nums[x+1:]:
-                return y
+        # for x, y in enumerate(nums):
+        #     if y not in nums[:x] + nums[x+1:]:
+        #         return y
+        i = 0
+        for x in nums:
+            i ^= x
+        return i
 
 if __name__ == '__main__':
     nums =[2,2,1]
-    Solution().singleNumber(nums)
+    print(Solution().singleNumber(nums))
