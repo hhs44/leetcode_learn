8bc82d1280774506857fda9323f79b6b9a90dc1c
huang <1159986871@qq.com>
2020-05-26 19:41:26 +0800

    287. 寻找重复数

--- 每日打卡一题/146. LRU缓存机制.py
+++ 每日打卡一题/146. LRU缓存机制.py
@@ -36,7 +36,7 @@ class ListNode:
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.hashmap = {}
+        self.dic = {}
         # 新建两个节点 head 和 tail
         self.head = ListNode()
         self.tail = ListNode()
@@ -47,15 +47,15 @@ class LRUCache:
     # 因为get与put操作都可能需要将双向链表中的某个节点移到末尾，所以定义一个方法
     def move_node_to_tail(self, key):
             # 先将哈希表key指向的节点拎出来，为了简洁起名node
-            #      hashmap[key]                               hashmap[key]
+            #      dic[key]                               dic[key]
             #           |                                          |
             #           V              -->                         V
             # prev <-> node <-> next         pre <-> next   ...   node
-            node = self.hashmap[key]
+            node = self.dic[key]
             node.prev.next = node.next
             node.next.prev = node.prev
             # 之后将node插入到尾节点前
-            #                 hashmap[key]                 hashmap[key]
+            #                 dic[key]                 dic[key]
             #                      |                            |
             #                      V        -->                 V
             # prev <-> tail  ...  node                prev <-> node <-> tail
@@ -65,32 +65,32 @@ class LRUCache:
             self.tail.prev = node
 
     def get(self, key: int) -> int:
-        if key in self.hashmap:
+        if key in self.dic:
             # 如果已经在链表中了久把它移到末尾（变成最新访问的）
             self.move_node_to_tail(key)
-        res = self.hashmap.get(key, -1)
+        res = self.dic.get(key, -1)
         if res == -1:
             return res
         else:
             return res.value
 
     def put(self, key: int, value: int) -> None:
-        if key in self.hashmap:
+        if key in self.dic:
             # 如果key本身已经在哈希表中了就不需要在链表中加入新的节点
             # 但是需要更新字典该值对应节点的value
-            self.hashmap[key].value = value
+            self.dic[key].value = value
             # 之后将该节点移到末尾
             self.move_node_to_tail(key)
         else:
-            if len(self.hashmap) == self.capacity:
+            if len(self.dic) == self.capacity:
                 # 去掉哈希表对应项
-                self.hashmap.pop(self.head.next.key)
+                self.dic.pop(self.head.next.key)
                 # 去掉最久没有被访问过的节点，即头节点之后的节点
                 self.head.next = self.head.next.next
                 self.head.next.prev = self.head
             # 如果不在的话就插入到尾节点前
             new = ListNode(key, value)
-            self.hashmap[key] = new
+            self.dic[key] = new
             new.prev = self.tail.prev
             new.next = self.tail
             self.tail.prev.next = new
