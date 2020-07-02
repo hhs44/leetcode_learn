8894dd6da910af1e8dcc63bec7b43fa84a563ca0
huang <1159986871@qq.com>
2020-07-02 08:36:29 +0800

    updates site 2020年07月 2日  8:36:29

---
+++ 718. 最长重复子数组.py
@@ -0,0 +1,56 @@
+from typing import List
+
+"""
+给两个整数数组 A 和 B ，返回两个数组中公共的、长度最长的子数组的长度。
+
+示例 1:
+
+输入:
+A: [1,2,3,2,1]
+B: [3,2,1,4,7]
+输出: 3
+解释:
+长度最长的公共子数组是 [3, 2, 1]。
+"""
+
+class Solution:
+    def findLength(self, A: List[int], B: List[int]) -> int:
+
+        # 动态规划
+        n, m = len(A), len(B)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        ans = 0
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                dp[i][j] = dp[i + 1][j + 1] + 1 if A[i] == B[j] else 0
+                print(dp)
+                ans = max(ans, dp[i][j])
+        return ans
+
+
+    # 优化滑动窗口
+        def maxLength(addA: int, addB: int, length: int) -> int:
+            ret = k = 0
+            for i in range(length):
+                if A[addA + i] == B[addB + i]:
+                    k += 1
+                    ret = max(ret, k)
+                else:
+                    k = 0
+            return ret
+
+        n, m = len(A), len(B)
+        ret = 0
+        for i in range(n):
+            length = min(m, n - i)
+            ret = max(ret, maxLength(i, 0, length))
+        for i in range(m):
+            length = min(n, m - i)
+            ret = max(ret, maxLength(0, i, length))
+        return ret
+
+
+if __name__ == '__main__':
+    A = [1,2,3,4,2,1]
+    B = [3,4,2,1,2,1]
+    print(Solution().findLength(A,B))
