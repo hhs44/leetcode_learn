9e29c5de91488630edb4bc92e612662689a10f46
RRtp <32462634+hhs44@users.noreply.github.com>
2020-07-30 14:28:50 +0800

    updates site 2020年07月30日 14:28:49

---
+++ 343.整数拆分.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=343 lang=python3
+#
+# [343] 整数拆分
+#
+
+# @lc code=start
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n <= 3: return n - 1
+        a, b = n // 3, n % 3
+        if b == 0: return int(math.pow(3, a))
+        if b == 1: return int(math.pow(3, a - 1) * 4)
+        return int(math.pow(3, a) * 2)
+
+# @lc code=end
+
--- 350.两个数组的交集-ii.py
+++ 350.两个数组的交集-ii.py
@@ -9,5 +9,6 @@ from collections import Counter
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return [*(Counter(nums1) & Counter(nums2)).elements()]
+    
 # @lc code=end
 
