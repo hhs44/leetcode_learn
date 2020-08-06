f4e7212b163fcb6b85a6f8714b2d1d7b0bce5315
RRtp <32462634+hhs44@users.noreply.github.com>
2020-08-06 16:43:51 +0800

    updates site 2020年08月 6日 16:43:50

---
+++ 336.回文对.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=336 lang=python3
+#
+# [336] 回文对
+#
+
+# @lc code=start
+class Solution:
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
+        
+# @lc code=end
+
